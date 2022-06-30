2b8aa7bc60caf8dc45ec6b408d6ad013d26e5739
VTNoble <noble.jason91@gmail.com>
2022-06-30 17:11:33 -0400

    added jupyter notebook and sql script file

---
+++ queries.sql
@@ -0,0 +1,14 @@
+-- Create table for dataframe to be loaded into
+CREATE TABLE home_sales (
+address TEXT,
+county TEXT,
+state TEXT,
+sale_date TEXT,
+sale_year INT,
+sale_month TEXT,
+sale_price INT,
+lat FLOAT(5),
+long FLOAT(5),
+block INT
+);
+
