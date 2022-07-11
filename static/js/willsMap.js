b177c72702dd051ce348136e0ad6cdd93a8ef676
wwright21 <williamcwrightjr@gmail.com>
2022-07-11 19:17:49 -0400

    updated index.html file

--- static/js/willsMap.js
+++ static/js/willsMap.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([33.799837439886055, -84.41356284230015], 8);
+var map = L.map('map').setView([33.799837439886055, -84.41356284230015], 9);
 
 var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
