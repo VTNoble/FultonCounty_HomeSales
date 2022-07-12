9c7239126fc7050bd50c6881189ce60e647e6ea4
wwright21 <williamcwrightjr@gmail.com>
2022-07-11 22:26:36 -0400

    got map working with event listener

--- static/js/willsMap.js
+++ static/js/willsMap.js
@@ -1,18 +1,57 @@
 var map = L.map('map').setView([33.799837439886055, -84.41356284230015], 9);
 
-var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
+    
+function createMap(){
+    
 
-url = "/api/v1.0/chloropleth"
+    // function to create color breaks
+    function getColor(d) {
+        return d > 800000 ? '#006d2c' :
+            d > 500000  ? '#31a354' :
+            d > 350000   ? '#74c476' :
+            d > 250000   ? '#bae4b3' :
+            d > 100000   ? '#edf8e9' :
+                        '#FFFFFF';
+    }
 
-d3.json(url).then(function(data) {
+    // function to style the individual blocks
+    function style(feature) {
+        let year = parseInt(d3.select('#selDataset').property("value"))
+        let salesData = feature.properties.sales.filter(d => {
+            return d.sale_year == year
+        })[0]
+        return {
+            fillColor: getColor(salesData.sale_price),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.8
+        };
+    }
 
-    console.log(data);
-    L.geoJson(data).addTo(map);
+    url = "/api/v1.0/chloropleth"
 
+    d3.json(url).then(function(data) {
+
+        console.log(data);
+        
+
+        L.geoJson(data, {style: style}).addTo(map);
+    })
+
+}
+
+// initialize the page
+createMap()
+
+// event listener for dropdown
+d3.select('#selDataset').on('change', () => {
+    console.log('event listener was clicked')
+    createMap()
 })
-
-
