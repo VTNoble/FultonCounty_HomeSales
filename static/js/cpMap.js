68302e2fed06ab777cc4a1e86ba89d0d18a295b8
wwright21 <williamcwrightjr@gmail.com>
2022-07-12 21:56:03 -0400

    changed name of javascript mapper

--- static/js/cpMap.js
+++ static/js/cpMap.js
@@ -1,9 +1,11 @@
+// declare map object
 var map = L.map('map').setView([33.799837439886055, -84.41356284230015], 9);
 
-    var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+// declare open street map tiles
+var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 
 function createMap(){
