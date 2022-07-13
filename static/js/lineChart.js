4560801c6ce9000c479ce2eca774554201814966
VTNoble <noble.jason91@gmail.com>
2022-07-12 22:22:58 -0400

    updated js for bar and line charts

--- static/js/lineChart.js
+++ static/js/lineChart.js
@@ -1,60 +1,60 @@
 // Fetch data from JSON
-async function fetchJSON() {
-  const url = 'http://127.0.0.1:5000/api/v1.0/median';
-  const response = await fetch(url);
+async function fetchlineJSON() {
+  let url = 'http://127.0.0.1:5000/api/v1.0/median';
+  let response = await fetch(url);
   // loads waiting to complete the request...
-  const datapoints = await response.json();
+  let datapoints = await response.json();
   return datapoints;
 };
 
-fetchJSON().then(datapoints => {
-  let year = parseInt(d3.select('#selDataset').property("value"));
-  filteredData = datapoints.filter(function(item){
-    return item.sale_year == year
+function createLine() {
+
+  fetchlineJSON().then(datapoints => {
+    let year = parseInt(d3.select('#selDataset').property("value"));
+    filteredData = datapoints.filter(function(item){
+      return item.sale_year == year
+    })
+    console.log(filteredData)
+    let month = filteredData.map(function(index){
+      return index.sale_month;
+    })
+    let medianPrice = filteredData.map(function(index){
+      return index.sale_price;
+    })
+
+    let data = {
+      labels: month,
+      datasets: [{
+        label: 'Median Home Value ($)',
+        backgroundColor: "#008000",
+        borderColor: "#008000",
+        data: medianPrice,
+      }]
+    };
+
+    let config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+
+    let chartStatus = Chart.getChart("lineChart"); // <canvas> id
+    if (chartStatus != undefined) {
+      chartStatus.destroy();
+      };
+
+    let lineChart = new Chart(
+      document.getElementById('lineChart'),
+      config
+    );  
   })
-  console.log(filteredData)
-  const month = filteredData.map(function(index){
-    return index.sale_month;
-  })
-  const medianPrice = filteredData.map(function(index){
-    return index.sale_price;
-  })
+}
 
-  // const labels = [
-  //   'January',
-  //   'February',
-  //   'March',
-  //   'April',
-  //   'May',
-  //   'June',
-  //   'July',
-  //   'August',
-  //   'September',
-  //   'October',
-  //   'November',
-  //   'December'
-  // ];
+// initalize the page
+createLine()
 
-  const data = {
-    labels: month,
-    datasets: [{
-      label: 'Median Home Value ($)',
-      backgroundColor: "#008000",
-      borderColor: "#008000",
-      data: medianPrice,
-    }]
-  };
-
-  const config = {
-    type: 'line',
-    data: data,
-    options: {}
-  };
-
-  const lineChart = new Chart(
-    document.getElementById('lineChart'),
-    config
-  );  
+// event listener for dropdown
+d3.select('#selDataset').on('change', () => {
+  console.log('event listener was clicked')
+  createLine()
 })
-
-
